2eb50848eae549c049ca7f6cfbc5891680c6c926
Yauris12 <alexisaube28@gmail.com>
2021-07-17 02:16:17 -0500

    pinterest actt

--- js/script.js
+++ js/script.js
@@ -1,10 +1,8 @@
 var elem = document.querySelector(".grid-container");
-imagesLoaded(elem, () => {
-    var msnry = new Masonry(elem, {
-        // options
-        itemSelector: ".container__fotografia",
-        columnWidth: 236,
-        gutter: 20,
-        isFitWidth: true,
-    });
+var msnry = new Masonry(elem, {
+    // options
+    itemSelector: ".container__fotografia",
+    columnWidth: 236,
+    gutter: 20,
+    isFitWidth: true,
 });
